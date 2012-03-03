e1ecab7881a259bf27a18fa85f3fed55d036ff6a
Katelyn Thode <katelyn@mumble-17.cs.wisc.edu>
2012-03-03 15:16:33 -0600

    register file compiles

--- h3/hw3_3/rf.v
+++ h3/hw3_3/rf.v
@@ -15,74 +15,91 @@ module rf (
    output [15:0] read2data;
    output        err;
    
-   wire [15:0] readData [7:0];
-   wire [15:0] writeRegData [7:0];
+  // wire [15:0] readData [7:0];
+  // wire [15:0] writeRegData [7:0];
    
-   reg rf8 [7:0] (.data(writeRegData), .out(readData), .clk(clk), .rst(rst)); 
+   wire [15:0] readData0, readData1, readData2, readData3, readData4, readData5, readData6, readData7;
+   wire [15:0] writeData0, writeData1, writeData2, writeData3, writeData4, writeData5, writeData6, writeData7;
    
-   writeRegData[0] = (write && (writeregsel == 3'b000)) ? writedata : writeRegData[0];
-   writeRegData[1] = (write && (writeregsel == 3'b001)) ? writedata : writeRegData[1];
-   writeRegData[2] = (write && (writeregsel == 3'b010)) ? writedata : writeRegData[2];
-   writeRegData[3] = (write && (writeregsel == 3'b011)) ? writedata : writeRegData[3];
-   writeRegData[4] = (write && (writeregsel == 3'b100)) ? writedata : writeRegData[4];
-   writeRegData[5] = (write && (writeregsel == 3'b101)) ? writedata : writeRegData[5];
-   writeRegData[6] = (write && (writeregsel == 3'b110)) ? writedata : writeRegData[6];
-   writeRegData[7] = (write && (writeregsel == 3'b111)) ? writedata : writeRegData[7]; 
+   reg [15:0] outData1, outData2;
+   
+   assign read1data = outData1;
+   assign read2data = outData2;
+      
+  // register rf8 [7:0] (.data(writeRegData), .out(readData), .clk(clk), .rst(rst)); 
+   
+   register r0 (.data(writeData0), .out(readData0), .clk(clk), .rst(rst));
+   register r1 (.data(writeData1), .out(readData1), .clk(clk), .rst(rst));
+   register r2 (.data(writeData2), .out(readData2), .clk(clk), .rst(rst));
+   register r3 (.data(writeData3), .out(readData3), .clk(clk), .rst(rst));
+   register r4 (.data(writeData4), .out(readData4), .clk(clk), .rst(rst));
+   register r5 (.data(writeData5), .out(readData5), .clk(clk), .rst(rst));
+   register r6 (.data(writeData6), .out(readData6), .clk(clk), .rst(rst));
+   register r7 (.data(writeData7), .out(readData7), .clk(clk), .rst(rst));
+   
+   assign writeData0 = (write && (writeregsel == 3'b000)) ? writedata : writeData0;
+   assign writeData1 = (write && (writeregsel == 3'b001)) ? writedata : writeData1;
+   assign writeData2 = (write && (writeregsel == 3'b010)) ? writedata : writeData2;
+   assign writeData3 = (write && (writeregsel == 3'b011)) ? writedata : writeData3;
+   assign writeData4 = (write && (writeregsel == 3'b100)) ? writedata : writeData4;
+   assign writeData5 = (write && (writeregsel == 3'b101)) ? writedata : writeData5;
+   assign writeData6 = (write && (writeregsel == 3'b110)) ? writedata : writeData6;
+   assign writeData7 = (write && (writeregsel == 3'b111)) ? writedata : writeData7; 
 
 
 always@(*)
 case (read1regsel)
   3'b000: begin
-  	  read1data <= readData[0];
+  	  outData1 = readData0;
   	  end
   3'b001: begin
-          read1data <= readData[1]; 
+          outData1 = readData1; 
   	  end
   3'b010: begin
-   	  read1data <= readData[2];  	   
+   	  outData1 = readData2;  	   
   	  end
   3'b011: begin
-   	  read1data <= readData[3]; 
+   	  outData1 = readData3; 
   	  end 
   3'b100: begin
-          read1data <= readData[4];  
+          outData1 = readData4;  
   	  end
   3'b101: begin
-  	  read1data <= readData[5];
+  	  outData1 = readData5;
   	  end
   3'b110: begin
-          read1data <= readData[6];
+          outData1 = readData6;
   	  end
   3'b111: begin
-  	  read1data <= readData[7];
+  	  outData1 = readData7;
   	  end
  endcase
  
  always@(*)
  case (read2regsel)
   3'b000: begin
-  	  read2data <= readData[0];
+  	  outData2 = readData0;
   	  end
   3'b001: begin
-          read2data <= readData[1]; 
+          outData2 = readData1; 
   	  end
   3'b010: begin
-   	  read2data <= readData[2];  	   
+   	  outData2 = readData2;  	   
   	  end
   3'b011: begin
-   	  read2data <= readData[3]; 
+   	  outData2 = readData3; 
   	  end 
   3'b100: begin
-          read2data <= readData[4];  
+          outData2 = readData4;  
   	  end
   3'b101: begin
-  	  read2data <= readData[5];
+  	  outData2 = readData5;
   	  end
   3'b110: begin
-          read2data <= readData[6];
+          outData2 = readData6;
   	  end
   3'b111: begin
-  	  read2data <= readData[7];
+  	  outData2 = readData7;
   	  end
  endcase
 endmodule
