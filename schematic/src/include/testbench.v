ba8eaabf77e41da47fb9d720b7124c4a31d4329a
Mike Starr <go2starr@gmail.com>
2012-03-19 20:53:43 -0500

    Add testbench included macros

---
+++ schematic/src/include/testbench.v
@@ -0,0 +1,33 @@
+// Fancy printing
+`define C_OKGREEN "\033[92m"
+`define C_FAIL    "\033[91m"
+`define C_WARN    "\033[93m"
+`define C_ENDC    "\033[0m"
+`define C_BLUE    "\033[94m"
+
+`define PRINT_C(msg, c) $write(c); $write(msg); $write(`C_ENDC);
+`define WARN(msg) `PRINT_C(msg, `C_WARN);
+`define FAIL(msg) `PRINT_C(msg, `C_FAIL);
+`define OK(msg)   `PRINT_C(msg, `C_OKGREEN);
+
+`define DEBUG 0
+
+// Assertion with debug messages
+`define test(ex, got, msg) \
+if (ex !== got) begin    \
+      $display(`C_FAIL);   \
+      $write("ERR:  ");  \
+      $display(msg);       \
+      $display("  --> Expected: %d  Got: %d", ex, got); \
+      $write(`C_ENDC);  \
+      no_errs = no_errs + 1; \
+end else begin \
+     `OK("."); \
+end
+               
+// Info printing
+`define info(msg) if (`DEBUG) begin `PRINT_C(msg, `C_BLUE); $display(""); end
+
+// Clock advancing
+`define tic @(posedge clk); #3;
+
