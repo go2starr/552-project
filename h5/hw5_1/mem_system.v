f2998456149c50908e26169b89d127c6da4bc4cf
Katelyn <katelyn.thode@gmail.com>
2012-04-09 18:41:49 -0500

    instantiated a cache in mem_system.v

--- h5/hw5_1/mem_system.v
+++ h5/hw5_1/mem_system.v
@@ -28,10 +28,35 @@ module mem_system(/*AUTOARG*/
    parameter mem_type = 0;
    
    // your code here
+	wire dirty, valid, comp, write, valid_in, enable;
+	wire [4:0] tag_out, tag_in;
+	wire [7:0] index;
+	wire [2:0] offset;
 
    // You must pass the mem_type parameter 
    // and createdump inputs to the 
    // cache modules
+   cache #(0 + memtype) c0(
+									// Inputs
+									.enable(enable),
+								   .clk(clk), 
+								   .rst(rst), 
+								   .createdump(createdump),
+								   .tag_in(tag_in), 
+								   .index(index), 
+									.offset(offset), 
+								   .data_in(DataIn), 
+								   .comp(comp), 
+									.write(write), 
+									.valid_in(valid_in), 
+									// Outputs
+									.tag_out(tag_out), 
+									.data_out(DataOut), 
+									.hit(CacheHit), 
+									.dirty(dirty), 
+									.valid(valid), 
+									.err(err)
+									);
    
 endmodule // mem_system
 
