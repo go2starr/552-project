9fd6d15e737648f72a2c9e9a17acc5dee76fbb42
Katelyn <katelyn.thode@gmail.com>
2012-04-09 18:48:27 -0500

    changed spacing

--- h5/hw5_1/mem_system.v
+++ h5/hw5_1/mem_system.v
@@ -39,15 +39,15 @@ module mem_system(/*AUTOARG*/
    cache #(0 + memtype) c0(
 									// Inputs
 									.enable(enable),
-								   .clk(clk), 
-								   .rst(rst), 
-								   .createdump(createdump),
-								   .tag_in(tag_in), 
-								   .index(index), 
+						   .clk(clk), 
+						   .rst(rst), 
+						   .createdump(createdump),
+						   .tag_in(tag_in), 
+						   .index(index), 
 									.offset(offset), 
-								   .data_in(DataIn), 
-								   .comp(comp), 
-									.write(write), 
+						   .data_in(DataIn), 
+						   .comp(comp), 
+						  	.write(write), 
 									.valid_in(valid_in), 
 									// Outputs
 									.tag_out(tag_out), 
