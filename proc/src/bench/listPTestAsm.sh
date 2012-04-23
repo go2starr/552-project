92922d87c14cf05d2e1f9260627553faf9eeaf53
Mike Starr <go2starr@gmail.com>
2012-04-23 15:09:24 -0500

    Add super run-all and log testbench

--- proc/src/bench/listPTestAsm.sh
+++ proc/src/bench/listPTestAsm.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-for i in `cat $1`; do ./ptestAsm.sh $i | grep "\(SUCC\|FAIL\|PASS\)"; done;
+for i in `cat $1`; do
+    FILE=`echo $i | sed -E "s/.*\/(.*)(\/.*\.list)/\\1/"`.summary.log
+    echo Removing old log file: $FILE...
+    rm -f $FILE
+
+    echo # newline
+    for j in `cat $i`; do
+        echo Running tests for input file: $j...
+        ./ptestAsm.sh $j | grep "\(SUCC\|FAIL\|PASS\)" | tee -a $FILE
+    done
+done
