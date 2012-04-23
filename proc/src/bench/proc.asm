0dac40396ae33642a68bd25d1afb58e7a6f9badd
Katelyn <katelyn.thode@gmail.com>
2012-04-23 16:13:42 -0500

    commented .asm files

--- proc/src/bench/proc.asm
+++ proc/src/bench/proc.asm
@@ -1,25 +1,25 @@
 //      Basic pipeline tests
-        lbi     r0, 0x0
-        lbi     r1, 0x1
-        lbi     r2, 0x2
-        lbi     r3, 0x3
-        lbi     r4, 0x4
-        add     r4, 0x5
-        sub     r4, 0x1
-        and     r2, 0x4
-        slbi    r2, 0x7
-        lbi     r0, 0x0
-        subi    r0, r0, 0x8
-        bltz    r0, .label1      
+        lbi     r0, 0x0 // r0 <- 0
+        lbi     r1, 0x1 // r1 <- 1
+        lbi     r2, 0x2 // r2 <- 2
+        lbi     r3, 0x3 // r3 <- 3
+        lbi     r4, 0x4 // r4 <- 4
+        add     r4, 0x5 // r4 <- r4 + 5 == 9 && should stall
+        sub     r4, 0x1 // r4 <- r4 - 1 == 8 && should stall
+        and     r2, 0x4 // r2 <- r2 && 4 == 0
+        slbi    r2, 0x7 // r2 <- r2 << 8 | 7 == 7  && should stall
+        lbi     r0, 0x0 // r0 <- 0  
+        subi    r0, r0, 0x8  // r0 <-r0 - 8 == -8 && should stall
+        bltz    r0, .label1  // should jump to label1  && should stall  
         nop
         nop
         nop
         nop
 
         .label1
-        add r0, r2, r0
-        sub r1, r0, r0
-        bnez r1, .label1
+        add r0, r2, r0     // ro <- r0 + r2 == -1 
+        sub r1, r0, r0     // r1 <- r0 - r0 == 0 && should stall
+        bnez r1, .label1   // equal to zero, so shouldn't branch; halt
 
 
         halt 
--- proc/src/bench/proc2.asm
+++ proc/src/bench/proc2.asm
@@ -1,12 +1,12 @@
-      lbi   r4, 0x0
-      lbi   r3, 0x1
-      lb1   r2, 0x2
-      lbi   r1, 0x3
-      lb1   r0, 0x4
-      add   r5, r0, r1
-      sub   r6, r2, r3
-      beqz  r6, .label1
-      bnez  r6, .label2
+      lbi   r4, 0x0  // r4 <- 0
+      lbi   r3, 0x1  // r3 <- 1
+      lb1   r2, 0x2  // r2 <- 2
+      lbi   r1, 0x3  // r1 <- 3
+      lb1   r0, 0x4  // r0 <- 4
+      add   r5, r0, r1 // r5 <- r0 + r1 == 7 && should stall
+      sub   r6, r2, r3  // r6 <- r2 + r3 == 3
+      beqz  r6, .label1 // r6 == 3, so don't take branch
+      bnez  r6, .label2 // r6 == 3, so take branch
 
 
       .label1
@@ -16,11 +16,11 @@
 
 
       .label2
-      lbi   r2, 0x08
-      jal   .jroutine
-      add   r2, r2, r6
-      st    r6, r2, 0
-      halt
+      lbi   r2, 0x08    // r2 <- 8
+      jal   .jroutine   // jump to routine
+      add   r2, r2, r6  // r2 <- r2 + r6 == 3
+      st    r6, r2, 0   // addr (r2 + 0) <- r6 ; addr 0 <- 3
+      halt  // halt
       halt
       halt
       halt
@@ -28,6 +28,6 @@
 
 
       .jroutine
-      sub r2, r2, 0x1
-      bnez r2, .jroutine
-      ret  
+      sub r2, r2, 0x1   // while r2 != 0, r2 <- r2 - 1
+      bnez r2, .jroutine   // branch back
+      ret  // then return
