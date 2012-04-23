cd3905f8c11f583b00a1bf56564c590e6acd7e4a
Mike Starr <go2starr@gmail.com>
2012-04-22 22:13:05 -0500

    Add new testall script

--- proc/src/bench/552-testAll.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in `cat all.list`; do ./testAsm.sh $i | grep "\(SUCCESS\|FAIL\)"; done;
--- proc/src/bench/testAll.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-#  Run all testbenches
-# 
-
-
-for i in `find .. | grep ".*bench.v$" | grep -o "t_.*bench"`; do
-    vtest $i | tee -a test.out;
-done;
